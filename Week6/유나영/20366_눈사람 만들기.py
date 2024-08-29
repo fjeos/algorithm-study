9366e9f58c3a8ea43b6c18d51d1de491571fccba
nayoung <yny3533@naver.com>
2024-08-29 23:22:02 +0900

    Week 6 - Problem 3 완료

--- Week6/유나영/20366_눈사람 만들기.py
+++ Week6/유나영/20366_눈사람 만들기.py
@@ -1,37 +1,18 @@
-from collections import defaultdict
-from itertools import combinations
-
 N = int(input())
 snowballs = list(map(int, input().split()))
 snowballs.sort()
-sub = []
-snowman = list(combinations(snowballs, 2))
-for i in range(len(snowman)):
-    snowman[i] += snowman[i][0] + tuple(snowman[i][1])
-snowmen = list(combinations(snowman, 2))
-print(snowman)
-#print(snowmen)
-length = len(snowmen)
-height = []
-min_value = 1e9
-snowmen.sort()
+snowman = []
+for i in range(N):
+    for j in range(i + 1, N):
+        snowman.append((i, j, snowballs[i] + snowballs[j]))
 
-for i in range(length - 1):
-    dic = defaultdict(int)
-    head, body = snowmen[i][0], snowmen[i][1]
-    #print(f"head: {head}, body: {body}")
-    if body[0] not in head and body[1] not in head:
-        a, b = head[0], head[1]
-        x, y = body[0], body[1]
-        #print(f"a: {a}, b: {b}, x: {x}, y: {y}")
-        if min_value > abs((x + y) - (a + b)):
-            min_value = abs((x + y) - (a + b))
-    # dic[a], dic[b] = 1, 1
-    # if dic[x] != 1 and dic[y] != 1:
-    #     height.append(abs((a + b) - (x + y)))
+snowman.sort(key=lambda x: x[2])
+answer = 1e9
+length = len(snowman)
+for i in range(length-1):
+    elza_head, elza_body, elza_height = snowman[i]
+    anna_head, anna_body, anna_height = snowman[i+1]
+    if elza_head != anna_head and elza_body != anna_body and elza_head != anna_body and elza_body != anna_head:
+        answer = min(answer, anna_height - elza_height)
 
-
-print(min_value)
-
-for i in range(601):
-    print(i, end=" ")
+print(answer)
